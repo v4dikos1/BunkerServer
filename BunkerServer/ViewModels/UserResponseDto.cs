5e998e9e999fd4d02a33e24f4544dc5f48b33292
v4d1kos <v4dikos@yandex.ru>
2023-02-12 17:22:23 +0700

    Добавлена авторизация/регистрация

---
+++ BunkerServer/ViewModels/UserResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BunkerServer.ViewModels
+{
+    public class UserResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Nickname { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
+    }
+}
